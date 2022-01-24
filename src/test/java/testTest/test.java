b444ce1cb98cfbf02833ae60de9e5b81b28d67f2
akshay shende <ashende950@gmail.com>
2022-01-24 17:45:14 +0530

    commit

---
+++ src/test/java/testTest/test.java
@@ -0,0 +1,14 @@
+package testTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class test {
+	@Test
+	public void testInputIsEven(){
+	 assertTrue(Main.checkIfInputIsAnEvenNumber(23)); //
+	
+	 }
+}
